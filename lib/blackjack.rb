53b55b0a646c8e084bc4210a00caa29261a41448
Sonia Dumitru <sonya_dumitru@yahoo.com>
2019-02-12 18:30:19 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -66,7 +66,7 @@ def runner
   # code runner here
   # welcome the user
   welcome
-  # deal their first two cards 
+  # deal their first two cards
   card_total = initial_round
   until card_total > 21
     card_total = hit?(card_total)
